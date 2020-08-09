5ca99c86f76aa4b86f5b4ec20d354c68aa171752
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-08-09 19:26:33 +0530

    commit on redux

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -13,12 +13,65 @@ const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0 }) 
     }
 });
 
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
+
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+})
+
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+})
+
+const setStartDate = (startDate) => ({
+    type: 'START_DATE',
+    startDate
+})
+
+const setEndDate = (endDate) => ({
+    type: 'END_DATE',
+    endDate
+})
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, filters) => {
+    return expenses;
+}
+
 // Expense Reducer
 const expenseReducerDefaultState = [];
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
     switch(action.type){
         case 'ADD_EXPENSE':
-            return state.concat(action.expense)     //concat is used since acc. to principle of Redux, the state must be read-only
+            // return state.concat(action.expense)  ==>   concat is used since acc. to principle of Redux, the state must be read-only
+            return [...state, action.expense]
+        case 'REMOVE_EXPENSE':
+            return state.filter((it) => {
+                return it.id !== action.id
+            })
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                }
+            })
         default:
             return state;
     }
@@ -33,6 +86,31 @@ const filterReducerDefaultState = {
 }
 const filterReducer = (state = filterReducerDefaultState, action) => {
     switch(action.type){
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                ...action
+            }
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            }
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            }
+        case 'START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            }
+        case 'END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            }
         default:
             return state;
     }
@@ -47,10 +125,28 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 })
 
-store.dispatch(addExpense({ description: 'Rent', amount: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 1000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 50 }));
+// console.log(expenseOne)
+
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 100 }))
+
+store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+
+// store.dispatch(setStartDate());
+// store.dispatch(setStartDate(125));
+// store.dispatch(setEndDate(12500));
 
 const demoState = {
     expenses: [{
